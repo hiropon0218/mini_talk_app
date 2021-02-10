bcf573189f2ef667e6af2c25f81e5a733669a8ee
hiropon0218 <74945462+hiropon0218@users.noreply.github.com>
2021-02-10 21:29:50 +0900

    記述ミスの修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,11 @@
 class MessagesController < ApplicationController
   def new
-    @messages = Message.All
+    @messages = Message.all
     @message = Message.new
   end
 
   def create
     @message = Message.new(text: params[:message][:text])
   end
-  
+
 end
